16bcc00766e6771cf7abf310bc6926caa13e30fe
Derek Peacock <derek.peacock@yahoo.com>
2022-10-14 10:27:18 +0100

    Added Student

---
+++ src/Week3/Student.java
@@ -0,0 +1,18 @@
+package Week3;
+
+public class Student
+{
+    private String id;
+    private String fullName;
+
+    public Student(String id, String fullName) 
+    {
+        this.id = id;
+        this.fullName = fullName;
+    }
+
+    public void print()
+    {
+        System.out.println(id + " " + fullName);
+    }
+}
--- src/Week3/Week03.java
+++ src/Week3/Week03.java
@@ -1,5 +1,7 @@
 package Week3;
 
+import helpers.InputReader;
+
 public class Week03 
 {
     public static final String CONSOLE_CLEAR = "\033[H\033[2J";
@@ -15,6 +17,17 @@ public class Week03
         System.out.println();
         System.out.println(" by Nicholas Day and Derek Peacock");
         System.out.println();
+
+        createStudent();
+    }
+    private static void createStudent() 
+    {
+        System.out.println();
+        String id = InputReader.getString("Please enter your id > ");
+        String name = InputReader.getString("Please enter your name > ");
+        
+        Student derek = new Student(id, name);
+        derek.print();
     }     
     
 }
